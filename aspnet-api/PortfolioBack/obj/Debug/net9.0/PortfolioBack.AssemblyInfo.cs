ddd87283ea69674f68fb2e7c518d07aa442ac34a
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-08-05 16:38:17 +0200

    Update assembly information and regenerate binaries for PortfolioBack

--- aspnet-api/PortfolioBack/obj/Debug/net9.0/PortfolioBack.AssemblyInfo.cs
+++ aspnet-api/PortfolioBack/obj/Debug/net9.0/PortfolioBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea0669d3d84728c0fd3cefa0b4b5aadeef61734a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c97dfa1c53e2b40fd5d9cbd6146994da18dd20")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
