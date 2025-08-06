6f7cb92796c148251d79724f4ff290dc935a107d
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-08-06 09:13:59 +0200

    Refactor createClient to enforce Database type in Supabase client creation

--- nextjs-app/lib/supabase/server.ts
+++ nextjs-app/lib/supabase/server.ts
@@ -1,4 +1,4 @@
-
+import { Database } from "@/types/database.types";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -7,22 +7,20 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = async () => {
   const cookieStore = await cookies();
-  return createServerClient(
-    supabaseUrl!,
-    supabaseKey!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        async setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
-          } catch {
-            console.error("Error setting cookies in Supabase client creation");
-          }
-        },
+  return createServerClient<Database>(supabaseUrl!, supabaseKey!, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      async setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
+        } catch {
+          console.error("Error setting cookies in Supabase client creation");
+        }
       },
     },
-  );
+  });
 };
