9505c971de4860e054750a35a8a941fe97605663
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-08-18 12:31:24 +0200

    Fix typos and reorder properties in Database type definition

--- nextjs-app/types/database.types.ts
+++ nextjs-app/types/database.types.ts
@@ -7,7 +7,7 @@ export type Json =
   | Json[]
 
 export type Database = {
-  // Allows to automatically instanciate createClient with right options
+  // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
     PostgrestVersion: "12.2.3 (519615d)"
@@ -175,17 +175,17 @@ export type Database = {
       }
       search_articles: {
         Args: {
-          query_embedding: string
-          match_threshold: number
           match_count: number
+          match_threshold: number
+          query_embedding: string
         }
         Returns: {
-          id: number
-          slug: string
-          title: string
           description: string
+          id: number
           long_description: string
           similarity: number
+          slug: string
+          title: string
         }[]
       }
       sparsevec_out: {
