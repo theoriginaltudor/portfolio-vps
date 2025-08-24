6aba272fc2d3a7868a5c48e225abc08e8d1be6ac
tudorcaseru <135211856+tudorcaseru@users.noreply.github.com>
2025-08-24 09:38:25 +0200

    change backend port to 5000

--- nextjs-app/lib/utils/get-url.ts
+++ nextjs-app/lib/utils/get-url.ts
@@ -1,7 +1,7 @@
 import { ApiEndpoint } from "./api";
 
 const getApiUrl = (endpoint: ApiEndpoint) => {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5258";
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
   return `${baseUrl}${endpoint.startsWith("/") ? endpoint : "/" + endpoint}`;
 };
 
