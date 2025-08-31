6ee66b75a8e05310d084ebaaca9a64c1431b7e68
Tudor Caseru <theoriginaltudor@gmail.com>
2025-08-31 10:51:03 +0200

    feat: update search_articles function to include skills and modify ProjectSearchService to handle new structure

--- aspnet-api/PortfolioBack/DTOs/ProjectSearchResult.cs
+++ aspnet-api/PortfolioBack/DTOs/ProjectSearchResult.cs
@@ -2,10 +2,10 @@ namespace PortfolioBack.DTOs;
 
 public class ProjectSearchResult
 {
-  public int Id { get; set; }
   public string Slug { get; set; } = string.Empty;
   public string Title { get; set; } = string.Empty;
   public string Description { get; set; } = string.Empty;
   public string? LongDescription { get; set; }
   public double Similarity { get; set; }
+  public List<SkillGetDto> Skills { get; set; } = new();
 }
