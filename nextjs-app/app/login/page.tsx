9f0efbcfc1ff3d1034f80ec99692a373182968b5
Tudor Caseru <theoriginaltudor@gmail.com>
2025-09-15 09:51:10 +0200

    Implement code changes to enhance functionality and improve performance

--- nextjs-app/app/login/page.tsx
+++ nextjs-app/app/login/page.tsx
@@ -1,6 +1,9 @@
 import { Input } from '@/components/ui/input';
 import type { Metadata } from 'next';
-import { loginUser } from './actions';
+import {
+  loginUser,
+  // registerUser
+} from './actions';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { getUser } from '@/lib/utils/server';
@@ -8,7 +11,8 @@ import { getUser } from '@/lib/utils/server';
 export default async function LoginPage({
   searchParams,
 }: {
-  searchParams: { redirect?: string };
+  // In Next.js 15 (React 19), searchParams is provided as a Promise for streaming compat
+  searchParams: Promise<{ redirect?: string | string[] }>;
 }) {
   const user = await getUser();
   if (user) {
@@ -20,7 +24,8 @@ export default async function LoginPage({
       </div>
     );
   }
-  const redirect = searchParams.redirect;
+  const { redirect: rawRedirect } = await searchParams;
+  const redirect = Array.isArray(rawRedirect) ? rawRedirect[0] : rawRedirect;
   return (
     <div className='flex flex-row gap-10'>
       <form
@@ -61,40 +66,40 @@ export default async function LoginPage({
         <Button type='submit'>Log in</Button>
       </form>
       {/* <form
-        className="flex flex-col gap-6 p-4 justify-center items-center mx-auto"
+        className='mx-auto flex flex-col items-center justify-center gap-6 p-4'
         action={registerUser}
       >
-        <div className="flex gap-2 w-full">
-          <Label htmlFor="username" className="flex-1 justify-end">
+        <div className='flex w-full gap-2'>
+          <Label htmlFor='username' className='flex-1 justify-end'>
             Username:
           </Label>
           <Input
-            className="w-[200px]"
-            id="username"
-            name="username"
-            type="text"
+            className='w-[200px]'
+            id='username'
+            name='username'
+            type='text'
             required
-            autoComplete="username"
+            autoComplete='username'
             minLength={3}
             maxLength={254}
           />
         </div>
-        <div className="flex gap-2 w-full">
-          <Label htmlFor="password" className="flex-1 justify-end">
+        <div className='flex w-full gap-2'>
+          <Label htmlFor='password' className='flex-1 justify-end'>
             Password:
           </Label>
           <Input
-            className="w-[200px]"
-            id="password"
-            name="password"
-            type="password"
+            className='w-[200px]'
+            id='password'
+            name='password'
+            type='password'
             required
             minLength={6}
             maxLength={128}
-            autoComplete="current-password"
+            autoComplete='current-password'
           />
         </div>
-        <Button type="submit">Sign up</Button>
+        <Button type='submit'>Sign up</Button>
       </form> */}
     </div>
   );
