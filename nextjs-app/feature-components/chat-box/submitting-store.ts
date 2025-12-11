5b3f898e15b61c84ad22cb8846915b1244ad3815
Tudor Caseru <theoriginaltudor@gmail.com>
2025-12-11 10:07:27 +0100

    eslint false positive

--- nextjs-app/feature-components/chat-box/submitting-store.ts
+++ nextjs-app/feature-components/chat-box/submitting-store.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 
 interface SubmittingStoreState {
   loading: boolean;
+  // eslint-disable-next-line no-unused-vars
   setLoading: (loading: boolean) => void;
 }
 
